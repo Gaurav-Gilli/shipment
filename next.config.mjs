bc30c57395f593bfe56944d384238a689efbfcb6
Gaurav-Gilli <77507126+Gaurav-Gilli@users.noreply.github.com>
2025-02-17 23:43:33 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async redirects() {
+      return [
+        {
+          source: '/',
+          destination: '/admin',  // Redirect root (/) to /admin
+          permanent: true,        // Permanent redirect (301)
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
